e2cd5d0494e26c219e72fa0872170b4b6db2e636
zaidi1997 <17053109@myrp.edu.sg>
2019-05-21 12:47:03 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/p05_ps/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.p05_ps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
