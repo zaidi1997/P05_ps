0efa27754340cd3aace9faceb6023c3756dff565
knightaur <francisanthonytoha@gmail.com>
2019-05-21 13:47:46 +0800

    1st commit

---
+++ app/src/main/java/sg/edu/rp/c346/p05_ps/EditActivity.java
@@ -0,0 +1,30 @@
+package sg.edu.rp.c346.p05_ps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class EditActivity extends AppCompatActivity {
+
+    Button btnCancel, btnDelete, btnUpdate;
+    EditText etID, etTitle, etSinger, etYear;
+    RadioGroup radGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        btnCancel = findViewById(R.id.btnCancel);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        etID = findViewById(R.id.etID);
+        etTitle = findViewById(R.id.etTitle);
+        etSinger = findViewById(R.id.etSinger);
+        etYear = findViewById(R.id.etYear);
+        radGroup = findViewById(R.id.radGroup);
+
+    }
+}
